30806b9a675356c5b91a7370e77e748d1d751fad
saj404 <48991692+saj404@users.noreply.github.com>
2021-06-27 11:27:20 +0800

    feat: added pseudo code

---
+++ script.js
@@ -0,0 +1,16 @@
+/*
+
+User clicks button number display on main
+if  user initial click not button number, nothing happens
+
+Once user clicks button operator, move number and operator to top display, get first operand and operator
+
+(if user clicks back button, erase last number input, if no number on main, nothing happens)
+(if user clicks clear, remove everything on display)
+(if customer clicked ., check if operand already has ., if true nothing happens, if false add .)
+
+if user did not enter a number yet but clicked another operator, replace previous operator with new one. If user already has a number entered, perform operation and make it first operand and get the new operator.
+
+if user entered second operaand and clicked equal, perform operation and end process.
+
+*/
