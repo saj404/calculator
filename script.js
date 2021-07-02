cb09fc33cba8a856350a6f8e5818eb68efb9f1cd
saj404 <48991692+saj404@users.noreply.github.com>
2021-07-02 12:01:54 +0800

    added eventlistener to buttons

--- script.js
+++ script.js
@@ -11,6 +11,31 @@ Once user clicks button operator, move number and operator to top display, get f
 
 if user did not enter a number yet but clicked another operator, replace previous operator with new one. If user already has a number entered, perform operation and make it first operand and get the new operator.
 
-if user entered second operaand and clicked equal, perform operation and end process.
+if user entered second operand and clicked equal, perform operation and end process.
 
 */
+
+let buttons = document.querySelectorAll('#main-body .button-cntnr');
+let mainDisplay = document.querySelector('#main-display');
+let firstOperand = '';
+let secondOperand = '';
+
+buttons.forEach( (button) => {
+    button.addEventListener('click', () => {
+        operate(button);
+    });
+});
+
+function operate(button) {
+    if(button.dataset.type == "number" && firstOperand.length <= 13) {
+        if(button.dataset.value == "." && !firstOperand.includes(".")) {
+            firstOperand += button.dataset.value;
+            mainDisplay.querySelector('span').textContent += button.dataset.value;
+            console.log(button.dataset.value);
+        } else if(button.dataset.value != ".") {
+            firstOperand += button.dataset.value;
+            mainDisplay.querySelector('span').textContent += button.dataset.value;
+            console.log(button.dataset.value);
+        }
+    }
+}
