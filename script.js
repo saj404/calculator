d10de9236ebeafd8ca5b3163dfbc0f40e7813029
saj404 <48991692+saj404@users.noreply.github.com>
2021-07-04 21:59:30 +0800

    added rough test operator function

--- script.js
+++ script.js
@@ -16,9 +16,11 @@ if user entered second operand and clicked equal, perform operation and end proc
 */
 
 let buttons = document.querySelectorAll('#main-body .button-cntnr');
-let mainDisplay = document.querySelector('#main-display');
+let mainDisplay = document.querySelector('#main-display span');
+let topDisplay = document.querySelector('#top-display span');
 let firstOperand = '';
 let secondOperand = '';
+let selectedOprtr = '';
 
 buttons.forEach( (button) => {
     button.addEventListener('click', () => {
@@ -27,15 +29,31 @@ buttons.forEach( (button) => {
 });
 
 function operate(button) {
-    if(button.dataset.type == "number" && firstOperand.length <= 13) {
-        if(button.dataset.value == "." && !firstOperand.includes(".")) {
-            firstOperand += button.dataset.value;
-            mainDisplay.querySelector('span').textContent += button.dataset.value;
-            console.log(button.dataset.value);
-        } else if(button.dataset.value != ".") {
-            firstOperand += button.dataset.value;
-            mainDisplay.querySelector('span').textContent += button.dataset.value;
-            console.log(button.dataset.value);
-        }
+    console.log(mainDisplay.textContent.length)
+    if(mainDisplay.textContent.length == 14) {
+        mainDisplay.style.fontSize = '.9em';
+    }
+    switch(button.dataset.type) {
+        case "number":
+            if(firstOperand.length <= 16) {
+                if(button.dataset.value == "." && !firstOperand.includes(".")) {
+                    firstOperand += button.dataset.value;
+                    mainDisplay.textContent += button.dataset.value;
+                } else if(button.dataset.value != ".") {
+                    firstOperand += button.dataset.value;
+                    mainDisplay.textContent += button.dataset.value;
+                }
+            }
+            break;
+        case "operator":
+            if(button.dataset.value == "%") {
+                let operandLength = mainDisplay.textContent.length
+                if(mainDisplay.textContent.charAt(operandLength - 1) == ".") {
+                    mainDisplay.textContent = mainDisplay.textContent.slice(0, operandLength - 1);
+                    firstOperand = mainDisplay.textContent.slice(0, operandLength - 1);
+                }
+                topDisplay.textContent = mainDisplay.textContent;
+                break;
+            }
     }
 }
